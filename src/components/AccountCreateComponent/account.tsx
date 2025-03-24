8b409458cd92ba2c0b051e79004892bb0cc9ba01
PauloHenriqueM-S <phttm0501@gmail.com>
2025-03-24 09:05:54 -0400

    Header, login/create account/recovery password screen

---
+++ src/components/AccountCreateComponent/account.tsx
@@ -0,0 +1,25 @@
+export default function Account() {
+  return (
+    <>
+    <div className="grid place-items-center mt-2">
+      <div className="flex items-center justify-center flex-col w-[50vh] h-[80vh] text-[#1F2B3D] text-center border-6 rounded-[20px] border-[#1F2B3D] border-solid mt-10 ">
+      <form action="post" className="flex flex-col p-0 m-0 ">
+        <label htmlFor="email" className="pb-2 pt-2">USERNAME</label>
+        <input type="text" id="username" name="username" className="border-2 rounded-[20px] border-[#1F2B3D] border-solid pb-2 "/>
+        <label htmlFor="email" className="pb-2 pt-2">EMAIL</label>
+        <input type="email" id="email" name="email" className="border-2 rounded-[20px] border-[#1F2B3D] border-solid pb-2 "/>
+        <label htmlFor="password" className="pb-2 pt-2">PASSWORD</label>
+        <input type="password" id="password" name="password" className="border-2 rounded-[20px] border-[#1F2B3D] border-solid pb-2" />
+        <label htmlFor="password" className="pb-2 pt-2">REPEAT PASSWORD</label>
+        <input type="password" id="repeat-password" name="repeat-password" className="border-2 rounded-[20px] border-[#1F2B3D] border-solid pb-2" />
+        <p id="uppercase" className="pb-2 pt-5 text-left">Least 1 upperacase</p>
+        <p id="specialCharacter" className="pb-2 pt-2 text-left">Least 1 special character</p>
+        <p id="number" className="pb-2 pt-2 text-left">Least 1 number</p>
+        <p id="characterLong" className="pb-2 pt-2 text-left">Least 6 character long</p>
+        <button type="submit" className="rounded-[20px] border-2 border-[#1F2B3D] border-solid bg-[#1F2B3D] text-white pb-2 pt-2 mt-5">Sign up</button>
+      </form>
+      </div>
+    </div>
+    </>
+  )
+}
---
+++ src/components/LoginComponent/login.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+export default function Login() {
+  return (
+    <>
+    <div className="grid place-items-center mt-40">
+      <div className="flex items-center justify-center flex-col w-[50vh] h-[50vh] text-[#1F2B3D] text-center border-6 rounded-[20px] border-[#1F2B3D] border-solid mt-10 ">
+      <form action="get" className="flex flex-col p-0 m-0 ">
+        <label htmlFor="email" className="pb-2 pt-2">USERNAME</label>
+        <input type="text" id="username" name="username" className="border-2 rounded-[20px] border-[#1F2B3D] border-solid pb-2 "/>
+        <label htmlFor="password" className="pb-2 pt-2">PASSWORD</label>
+        <input type="password" id="password" name="password" className="border-2 rounded-[20px] border-[#1F2B3D] border-solid pb-2" />
+        <button type="submit" className="rounded-[20px] border-2 border-[#1F2B3D] border-solid bg-[#1F2B3D] text-white pb-2 pt-2 mt-5">Sign in</button>
+      </form>
+      <div className="flex flex-col mt-10">
+      <Link href="/" className="mt-2 mb-2">I CAN&apos;T REMEMBER MY PASSWORD</Link>
+      <Link href="/" className="mt-2 mb-2">I DON&apos;T HAVE AN ACCOUNT!</Link>
+      </div>
+      </div>
+    </div>
+    </>
+  )
+}
