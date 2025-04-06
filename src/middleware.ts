f32da33b1c7bef0576b885db9407176564bfae4b
PauloHenriqueM-S <phttm0501@gmail.com>
2025-04-06 04:14:05 -0400

    middleware created

---
+++ src/middleware.ts
@@ -0,0 +1,57 @@
+import { NextResponse, type MiddlewareConfig, type NextRequest } from "next/server";
+
+const publicRoutes = [
+  { path: '/signin', whenAuthenticated: 'redirect' },
+  { path: '/signup', whenAuthenticated: 'redirect' },
+  { path: '/', whenAuthenticated: 'redirect' },
+] as const
+
+const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = "/signin";
+
+export function middleware(request: NextRequest){
+  const path = request.nextUrl.pathname;
+  const publicRoute = publicRoutes.find(route => route.path === path);
+  const authToken = request.cookies.get('token');
+
+if (!authToken && publicRoute){
+  return NextResponse.next()
+}
+
+if (!authToken && !publicRoute){
+  const redirectUrl = request.nextUrl.clone();
+
+  redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE;
+
+  return NextResponse.redirect(redirectUrl);
+}
+
+if (authToken && publicRoute && publicRoute.whenAuthenticated === 'redirect'){
+  const redirectUrl = request.nextUrl.clone();
+
+  redirectUrl.pathname = '/';
+
+  return NextResponse.redirect(redirectUrl);
+}
+
+if (authToken && !publicRoute){
+  // Checar se o JWT está EXPIRADO
+  // Se sim, remover o cookie e redirecionar o usuário para a tela de login
+  // Aplicar uma estratégia de refresh
+  return NextResponse.next()
+}
+
+  return NextResponse.next()
+}
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    /**
+    *  Match all request paths except for the ones starting with:
+    *  - api (API routes)
+    *  - _next/static (static files)
+    *  - _next/image (image optimization files)
+    *  - favicon.ico, sitmap.xml, robots.txt (metadata files)
+    */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ]
+}
